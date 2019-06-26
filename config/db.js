2b2d23c6f25f2aa6887631de67fa14d4f35423bc
ezopas <ezostrauskas@gmail.com>
2019-06-26 22:32:00 +0300

    added connection to database

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+        });
+
+        console.log('mongo connected');
+    }catch(err) {
+        console.log(err.message);
+
+        //exiting process if failure
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
