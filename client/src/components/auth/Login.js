ce22f515c1aaac8f69657aa3339e5cd9bf2ab7c5
ezopas <ezostrauskas@gmail.com>
2019-07-09 22:56:43 +0300

    Aded registration and login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,11 @@
 import React, {Fragment, useState} from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+import propTypes from 'prop-types';
+import {login} from '../../actions/auth';
 
-const Login = () => {
+const Login = ({login, isAuthenticated }) => {
     const [formData, serFormData] = useState({
         email: '',
         password: ''
@@ -14,8 +17,13 @@ const Login = () => {
 
     const onSubmit = async e => {
         e.preventDefault();
-        console.log('succses');
+        login(email, password);
     };
+
+    //redirect if logged in
+    if(isAuthenticated){
+        return <Redirect to="/dashboard"/>
+    }
 
     return (
         <Fragment>
@@ -43,4 +51,13 @@ const Login = () => {
     )
 };
 
-export default Login;
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps(), {login})(Login);
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {logout} from "../../actions/auth";
 
-const Navbar = () => {
+const Navbar = ({auth: {isAuthenticated, loading}, logout}) => {
+    const authLinks = (
+        <ul>
+            <li><a href="/profiles" onClick={logout}>Logout</a></li>
+        </ul>
+    );
+
+    const guestLinks = (
+        <ul>
+    <li><Link to="/profiles">Developers</Link></li>
+    <li><Link to="/register">Register</Link></li>
+    <li><Link to="/login">Login</Link></li>
+    </ul>
+    );
+
     return (
         <nav className="navbar bg-dark">
             <h1>
                 <Link to="index.html">LinkedCV</Link>
             </h1>
-            <ul>
-                <li><Link to="/profiles">Developers</Link></li>
-                <li><Link to="/register">Register</Link></li>
-                <li><Link to="/login">Login</Link></li>
-            </ul>
+            {!loading ? '<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>' : null}
         </nav>
     )
 };
 
-export default Navbar;
+Navbar.propTypes = {
+    logout: PropTypes.func.isRequired,
+    auth: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, {logout})(Navbar);
