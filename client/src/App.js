e64563ef0a4c3e67360b7cc489790bc09bf0b6fb
ezopas <ezostrauskas@gmail.com>
2019-07-09 22:56:43 +0300

    Aded registration and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect} from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -6,11 +6,22 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import {loadUser} from "./actions/auth";
+import setAuthToken from './utils/setAuthToken';
 //redux
 import {Provider} from 'react-redux';
 import store from './store';
+import setAuthToken from "./utils/setAuthToken";
+
+if(localStorage.token){
+    setAuthToken(localStorage.token);
+}
 
 const App = () => {
+    useEffect(() => {
+        store.dispatch(loadUser());
+    }, []);
+
   return (
       <Provider store={store}>
       <Router>
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,12 @@
 import React, {Fragment, useState} from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {setAlert} from "../../actions/alert";
 import PropTypes from 'prop-types';
+import {register} from "../../actions/auth";
 
-const Register = ({setAlert}) => {
+const Register = ({setAlert, register, isAuthenticated}) => {
     const [formData, serFormData] = useState({
         name: '',
         email: '',
@@ -20,8 +21,9 @@ const Register = ({setAlert}) => {
     const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2){
-            props,setAlert('Password do not mach!', 'danger')
+            setAlert('Password do not mach!', 'danger')
         }else {
+            register({name, email, password});
            // const newuser = {
            //     name,
            //     email,
@@ -42,6 +44,10 @@ const Register = ({setAlert}) => {
            // }
         }
     };
+
+    if(isAuthenticated){
+        return <Redirect to='/dashboard' />
+    }
 
     return (
         <Fragment>
@@ -86,7 +92,14 @@ const Register = ({setAlert}) => {
 };
 
 Register.propTypes = {
-    setAlert: PropTypes.func.isRequired
-}
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+};
 
-export default connect(null, {setAlert})(Register);
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+
+export default connect(mapStateToProps, {setAlert, register})(Register);
